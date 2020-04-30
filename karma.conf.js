ddc6e47aaa7f1588e644ff0d4a3aff6812266d05
Santosh Singh <sansingh28@publicisgroupe.net>
2020-04-30 18:48:55 +0530

    Adding Istanbul code coverage files

--- karma.conf.js
+++ karma.conf.js
@@ -10,23 +10,51 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      // Jasmine Karma Teamcity reporter
+      require('karma-teamcity-reporter')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/kashiya'),
+      dir: require('path').join(__dirname, './coverage/UT'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        // thresholds for all files
+        global: {
+          statements: 0,
+          lines: 0,
+          branches: 0,
+          functions: 0
+        },
+         // thresholds per file
+        each: {
+          statements: 0,
+          lines: 0,
+          branches: 0,
+          functions: 0
+        }
+      }
+    },    
+    reporters: ['progress', 'kjhtml'],    
+    jasmineHtmlReporter: {
+      // Jasmine Reporter Settings to Suppress all messages (overrides other suppress settings) failed messages
+      suppressAll: true, 
+      suppressFailed: true
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: 4203,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,  
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
   });
 };
